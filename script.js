5e54ce7d9d8f247b31f955b4588de8b4fc4bca3b
aidanmeehan12 <124737258+aidanmeehan12@users.noreply.github.com>
2024-08-04 21:13:58 +0000

    Implemented modals - but not text inside them

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function sendToAbout(){
 function sendToHome(){
   window.location="index.html"
 }
+
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "flex";
+
+    var span = modal.getElementsByClassName("close")[0];
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
